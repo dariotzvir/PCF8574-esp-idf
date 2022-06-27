715c3165115eff50c83ca79f52b36fc317144891
Dario Tzvir <dario.tzvir@it-one.com.ar>
2022-06-27 17:02:12 -0300

    .

--- PCF8574.cpp
+++ PCF8574.cpp
@@ -12,61 +12,57 @@
 
 #include "PCF8574.h"
 
-
-PCF8574::PCF8574(const uint8_t deviceAddress, TwoWire *wire)
+PCF8574::PCF8574(const uint8_t deviceAddress, const i2c_port_t port)
 {
-  _address    = deviceAddress;
-  _wire       = wire;
-  _dataIn     = 0;
-  _dataOut    = 0xFF;
-  _buttonMask = 0xFF;
-  _error      = PCF8574_OK;
+    _address    = deviceAddress;
+    _port       = port;
+    _dataIn     = 0;
+    _dataOut    = 0xFF;
+    _buttonMask = 0xFF;
+    _error      = PCF8574_OK;
 }
 
-
-#if defined (ESP8266) || defined(ESP32)
-bool PCF8574::begin(int dataPin, int clockPin, uint8_t value)
+bool PCF8574::begin(const i2c_config_t i2c_conf, uint8_t value)
 {
-  _wire = &Wire;
-  if ((dataPin < 255) && (clockPin < 255))
-  {
-    _wire->begin(dataPin, clockPin);
-  } else {
-    _wire->begin();
-  }
-  if (! isConnected()) return false;
-  PCF8574::write8(value);
-  return true;
-}
-#endif
+    _i2c_conf = i2c_conf;
+    i2c_param_config(_port, &_i2c_conf);   
+    i2c_driver_install(_port, I2C_MODE_MASTER, 0, 0, 0);
 
+    if (! isConnected()) return false;
+    PCF8574::write8(value);
+    return true;
+}
 
 bool PCF8574::begin(uint8_t value)
 {
-  _wire->begin();
-  if (! isConnected()) return false;
-  PCF8574::write8(value);
-  return true;
+    if (! isConnected()) return false;
+    PCF8574::write8(value);
+    return true;
 }
 
 
 bool PCF8574::isConnected()
 {
-  _wire->beginTransmission(_address);
-  return ( _wire->endTransmission() == 0);
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, _address << 1 | I2C_MASTER_WRITE, I2C_MASTER_ACK);
+    esp_err_t r = i2c_master_cmd_begin(I2C_NUM_0, cmd, 100 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+
+    return (r == ESP_OK);
 }
 
 
 bool PCF8574::setAddress(const uint8_t deviceAddress)
 {
-  _address = deviceAddress;
-  return isConnected();
+    _address = deviceAddress;
+    return isConnected();
 }
 
 
 uint8_t PCF8574::getAddress()
 {
-  return _address;
+    return _address;
 }
 
 
@@ -77,168 +73,183 @@ uint8_t PCF8574::getAddress()
 // TODO    @800 KHz -> ??
 uint8_t PCF8574::read8()
 {
-  if (_wire->requestFrom(_address, (uint8_t)1) != 1)
-  {
-    _error = PCF8574_I2C_ERROR;
-    return _dataIn; // last value
-  }
-  _dataIn = _wire->read();
-  return _dataIn;
-}
+    uint8_t buf[1] = {0};
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
 
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, _address << 1 | I2C_MASTER_READ, I2C_MASTER_ACK);
+    i2c_master_read_byte(cmd, buf, I2C_MASTER_ACK);
+    esp_err_t r = i2c_master_cmd_begin(_port, cmd, 100 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+    if (r != ESP_OK)
+    {
+        _error = PCF8574_I2C_ERROR;
+        return _dataIn; // last value
+    }
+
+    _dataIn = buf[0];
+    //ESP_LOGI("obj", "read8: %d", _dataIn);
+    return _dataIn;
+}
 
 void PCF8574::write8(const uint8_t value)
 {
-  _dataOut = value;
-  _wire->beginTransmission(_address);
-  _wire->write(_dataOut);
-  _error = _wire->endTransmission();
+    _dataOut = value;
+    //ESP_LOGI("obj", "write8: %d", _dataOut);
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, _address << 1 | I2C_MASTER_WRITE, I2C_MASTER_ACK);
+    i2c_master_write_byte(cmd, _dataOut, I2C_MASTER_ACK);
+    _error = i2c_master_cmd_begin(I2C_NUM_0, cmd, 100 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
 }
 
 
 uint8_t PCF8574::read(const uint8_t pin)
 {
-  if (pin > 7)
-  {
-    _error = PCF8574_PIN_ERROR;
-    return 0;
-  }
-  PCF8574::read8();
-  return (_dataIn & (1 << pin)) > 0;
+    if (pin > 7)
+    {
+        _error = PCF8574_PIN_ERROR;
+        return 0;
+    }
+    PCF8574::read8();
+    return (_dataIn & (1 << pin)) > 0;
 }
 
 
 void PCF8574::write(const uint8_t pin, const uint8_t value)
 {
-  if (pin > 7)
-  {
-    _error = PCF8574_PIN_ERROR;
-    return;
-  }
-  if (value == LOW)
-  {
-    _dataOut &= ~(1 << pin);
-  }
-  else
-  {
-    _dataOut |= (1 << pin);
-  }
-  write8(_dataOut);
+    if (pin > 7)
+    {
+        _error = PCF8574_PIN_ERROR;
+        return;
+    }
+    if (value == 0)
+    {
+        _dataOut &= ~(1 << pin);
+    }
+    else
+    {
+        _dataOut |= (1 << pin);
+    }
+    write8(_dataOut);
 }
 
 
 void PCF8574::toggle(const uint8_t pin)
 {
-  if (pin > 7)
-  {
-    _error = PCF8574_PIN_ERROR;
-    return;
-  }
-  toggleMask(1 << pin);
+    if (pin > 7)
+    {
+        _error = PCF8574_PIN_ERROR;
+        return;
+    }
+    toggleMask(1 << pin);
 }
 
 
 void PCF8574::toggleMask(const uint8_t mask)
 {
-  _dataOut ^= mask;
-  PCF8574::write8(_dataOut);
+    _dataOut ^= mask;
+    PCF8574::write8(_dataOut);
 }
 
 
 void PCF8574::shiftRight(const uint8_t n)
 {
-  if ((n == 0) || (_dataOut == 0)) return;
-  if (n > 7)         _dataOut = 0;     // shift 8++ clears all, valid...
-  if (_dataOut != 0) _dataOut >>= n;   // only shift if there are bits set
-  PCF8574::write8(_dataOut);
+    if ((n == 0) || (_dataOut == 0)) return;
+    if (n > 7)         _dataOut = 0;     // shift 8++ clears all, valid...
+    if (_dataOut != 0) _dataOut >>= n;   // only shift if there are bits set
+    PCF8574::write8(_dataOut);
 }
 
 
 void PCF8574::shiftLeft(const uint8_t n)
 {
-  if ((n == 0) || (_dataOut == 0)) return;
-  if (n > 7)         _dataOut = 0;    // shift 8++ clears all, valid...
-  if (_dataOut != 0) _dataOut <<= n;  // only shift if there are bits set
-  PCF8574::write8(_dataOut);
+    if ((n == 0) || (_dataOut == 0)) return;
+    if (n > 7)         _dataOut = 0;    // shift 8++ clears all, valid...
+    if (_dataOut != 0) _dataOut <<= n;  // only shift if there are bits set
+    PCF8574::write8(_dataOut);
 }
 
 
 int PCF8574::lastError()
 {
-  int e = _error;
-  _error = PCF8574_OK;  // reset error after read, is this wise?
-  return e;
+    int e = _error;
+    _error = PCF8574_OK;  // reset error after read, is this wise?
+    return e;
 }
 
 
 void PCF8574::rotateRight(const uint8_t n)
 {
-  uint8_t r = n & 7;
-  if (r == 0) return;
-  _dataOut = (_dataOut >> r) | (_dataOut << (8 - r));
-  PCF8574::write8(_dataOut);
+    uint8_t r = n & 7;
+    if (r == 0) return;
+    _dataOut = (_dataOut >> r) | (_dataOut << (8 - r));
+    PCF8574::write8(_dataOut);
 }
 
 
 void PCF8574::rotateLeft(const uint8_t n)
 {
-  rotateRight(8 - (n & 7));
+    rotateRight(8 - (n & 7));
 }
 
 
 void PCF8574::reverse() // quite fast: 14 shifts, 3 or, 3 assignment.
 {
-  uint8_t x = _dataOut;
-  x = (((x & 0xAA) >> 1) | ((x & 0x55) << 1));
-  x = (((x & 0xCC) >> 2) | ((x & 0x33) << 2));
-  x =          ((x >> 4) | (x << 4));
-  PCF8574::write8(x);
+    uint8_t x = _dataOut;
+    x = (((x & 0xAA) >> 1) | ((x & 0x55) << 1));
+    x = (((x & 0xCC) >> 2) | ((x & 0x33) << 2));
+    x =          ((x >> 4) | (x << 4));
+    PCF8574::write8(x);
 }
 
 
 //added 0.1.07/08 Septillion
 uint8_t PCF8574::readButton8(const uint8_t mask)
 {
-  uint8_t temp = _dataOut;
-  PCF8574::write8(mask | _dataOut);  // read only selected lines
-  PCF8574::read8();
-  PCF8574::write8(temp);             // restore
-  return _dataIn;
+    uint8_t temp = _dataOut;
+    PCF8574::write8(mask | _dataOut);  // read only selected lines
+    PCF8574::read8();
+    PCF8574::write8(temp);             // restore
+    return _dataIn;
 }
 
 
 //added 0.1.07 Septillion
 uint8_t PCF8574::readButton(const uint8_t pin)
 {
-  if (pin > 7)
-  {
-    _error = PCF8574_PIN_ERROR;
-    return 0;
-  }
+    if (pin > 7)
+    {
+        _error = PCF8574_PIN_ERROR;
+        return 0;
+    }
 
-  uint8_t temp = _dataOut;
-  PCF8574::write(pin, HIGH);
-  uint8_t value = PCF8574::read(pin);
-  PCF8574::write8(temp);
-  return value;
+    uint8_t temp = _dataOut;
+    //ESP_LOGI("obj", "temp %d", temp);
+    PCF8574::write(pin, 1);
+    uint8_t value = PCF8574::read(pin);
+    //ESP_LOGI("obj", "value %d", value);
+    PCF8574::write8(temp);
+    return value;
 }
 
 
 void PCF8574::select(const uint8_t pin)
 {
-  uint8_t n = 0x00;
-  if (pin < 8) n = 1 << pin;
-  write8(n);
+    uint8_t n = 0x00;
+    if (pin < 8) n = 1 << pin;
+    write8(n);
 };
 
 
 void PCF8574::selectN(const uint8_t pin) 
 {
-  uint8_t n = 0xFF;
-  if (pin < 8) n = (2 << pin) - 1;
-  write8(n);
+    uint8_t n = 0xFF;
+    if (pin < 8) n = (2 << pin) - 1;
+    write8(n);
 };
-
 
 
 // -- END OF FILE --
--- PCF8574.h
+++ PCF8574.h
@@ -1,4 +1,3 @@
-#pragma once
 //
 //    FILE: PCF8574.h
 //  AUTHOR: Rob Tillaart
@@ -7,89 +6,86 @@
 // PURPOSE: Arduino library for PCF8574 - 8 channel I2C IO expander
 //     URL: https://github.com/RobTillaart/PCF8574
 //          http://forum.arduino.cc/index.php?topic=184800
-//
-
-
-#include "Arduino.h"
-#include "Wire.h"
-
-
-#define PCF8574_LIB_VERSION         (F("0.3.5"))
+//    FORK: Tzvir Darío
 
 #ifndef PCF8574_INITIAL_VALUE
 #define PCF8574_INITIAL_VALUE       0xFF
-#endif
 
 #define PCF8574_OK                  0x00
 #define PCF8574_PIN_ERROR           0x81
 #define PCF8574_I2C_ERROR           0x82
+#define PCF8574_TIMEOUT_MS 50
 
-
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "driver/i2c.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
 class PCF8574
 {
 public:
-  explicit PCF8574(const uint8_t deviceAddress = 0x20, TwoWire *wire = &Wire);
-
-#if defined (ESP8266) || defined(ESP32)
-  bool    begin(int sda, int scl, uint8_t value = PCF8574_INITIAL_VALUE);
-#endif
-  bool    begin(uint8_t value = PCF8574_INITIAL_VALUE);
-  bool    isConnected();
+    PCF8574(const uint8_t deviceAddress, const i2c_port_t port);
 
 
-  // note: setting the address corrupt internal buffer values
-  // a read8() / write8() call updates them.
-  bool    setAddress(const uint8_t deviceAddress);
-  uint8_t getAddress();  
+    bool    begin(const i2c_config_t i2c_conf, uint8_t value);
+    bool    begin(uint8_t value);
+    bool    isConnected();
 
 
-  uint8_t read8();
-  uint8_t read(const uint8_t pin);
-  uint8_t value() const { return _dataIn; };
+    // note: setting the address corrupt internal buffer values
+    // a read8() / write8() call updates them.
+    bool    setAddress(const uint8_t deviceAddress);
+    uint8_t getAddress();  
 
 
-  void    write8(const uint8_t value);
-  void    write(const uint8_t pin, const uint8_t value);
-  uint8_t valueOut() const { return _dataOut; }
+    uint8_t read8();
+    uint8_t read(const uint8_t pin);
+    uint8_t value() const { return _dataIn; };
 
 
-  //added 0.1.07/08 Septillion
-  uint8_t readButton8()  { return PCF8574::readButton8(_buttonMask); }
-  uint8_t readButton8(const uint8_t mask);
-  uint8_t readButton(const uint8_t pin);
-  void    setButtonMask(const uint8_t mask) { _buttonMask = mask; };
-  uint8_t getButtonMask() { return _buttonMask; };
+    void    write8(const uint8_t value);
+    void    write(const uint8_t pin, const uint8_t value);
+    uint8_t valueOut() const { return _dataOut; }
 
 
-  // rotate, shift, toggle, reverse expect all lines are output
-  void    toggle(const uint8_t pin);
-  void    toggleMask(const uint8_t mask = 0xFF);    // default 0xFF ==> invertAll()
-  void    shiftRight(const uint8_t n = 1);
-  void    shiftLeft(const uint8_t n = 1);
-  void    rotateRight(const uint8_t n = 1);
-  void    rotateLeft(const uint8_t n = 1);
-  void    reverse();
+    //added 0.1.07/08 Septillion
+    uint8_t readButton8()  { return PCF8574::readButton8(_buttonMask); }
+    uint8_t readButton8(const uint8_t mask);
+    uint8_t readButton(const uint8_t pin);
+    void    setButtonMask(const uint8_t mask) { _buttonMask = mask; };
+    uint8_t getButtonMask() { return _buttonMask; };
 
 
-  void    select(const uint8_t pin);
-  void    selectN(const uint8_t pin);
-  void    selectNone() { write8(0x00); };
-  void    selectAll()  { write8(0xFF); };
+    // rotate, shift, toggle, reverse expect all lines are output
+    void    toggle(const uint8_t pin);
+    void    toggleMask(const uint8_t mask = 0xFF);    // default 0xFF ==> invertAll()
+    void    shiftRight(const uint8_t n = 1);
+    void    shiftLeft(const uint8_t n = 1);
+    void    rotateRight(const uint8_t n = 1);
+    void    rotateLeft(const uint8_t n = 1);
+    void    reverse();
 
 
-  int     lastError();
+    void    select(const uint8_t pin);
+    void    selectN(const uint8_t pin);
+    void    selectNone() { write8(0x00); };
+    void    selectAll()  { write8(0xFF); };
+
+
+    int     lastError();
 
 
 private:
-  uint8_t _address;
-  uint8_t _dataIn;
-  uint8_t _dataOut;
-  uint8_t _buttonMask;
-  int     _error;
+    uint8_t _address;
+    i2c_port_t _port = I2C_NUM_0;
+    uint8_t _dataIn;
+    uint8_t _dataOut;
+    uint8_t _buttonMask;
+    int     _error;
 
-  TwoWire*  _wire;
+    i2c_config_t _i2c_conf;
 };
 
-
+#endif
 // -- END OF FILE --
 
